109dc371155aa3495b89c115bbf6115f33d397bf
Yael Kaufman <ylkauf@gmail.com>
2023-02-03 13:23:10 -0800

    trying to figure out some js issues...still working on it

--- js/app.js
+++ js/app.js
@@ -16,78 +16,98 @@ const pastrami = document.getElementById("pastrami");
 const sprouts = document.getElementById("sprouts");
 const capers = document.getElementById("capers");
 const submitButton = document.getElementById("orderUp");
-const tip = document.getElementById("tip");
-const foodButtons = [bagel, tomato, lox, onion, avocado ,special, bacon, creamcheese, 
+const tip = document.getElementById("tiptext");
+const foodButtons = [bagel, tomato, lox, onion, avocado, special, bacon, creamcheese,
     swiss, egg, vegegg, usa, cheddar, pastrami, sprouts, capers];
-let ingredientsToPick=[];
-let chosenIngredients=[];
+let ingredientsToPick = [];
+let chosenIngredients = [];
 const workBoard = document.getElementById("current-ingredients");
 let currentTip = 0;
-
+let totalRounds = 0;
+let checkVal = false;
+let finalIngredients=[];
 const sandwiches = [
-    {name:'The Classic', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion']},
-    {name:'The Classic1', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion']},
-    {name:'The Classic2', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion']}
+    { name: 'The Classic', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion'] },
+    { name: 'The Basic', ingredients: ['Bagel', 'Cream Cheese'] },
+    { name: 'The Egg, Cheese & Bacon', ingredients: ['Bagel', 'Egg', 'American Cheese', 'Special Sauce', 'Avocado', 'Bacon'] },
+    { name: 'The Vegan', ingredients: ['Bagel', 'Avocado', 'Sprouts', 'Tomato', 'Onion', 'Vegan Egg'] },
+    { name: 'The Pastrami Breakfast', ingredients: ['Bagel', 'Pastrami', 'Egg', 'Swiss Cheese', 'Special Sauce'] }
 ]
 
 //chooses a sandwich from the list, prints its name and ingredients
-function chooseSandwich (){
+function chooseSandwich() {
     let choose = Math.floor(Math.random() * sandwiches.length);
-    console.log(choose);
     let choice = sandwiches[choose]; //picks sandwich, assigns to choice
     ingredientsToPick = choice.ingredients;
-    console.log("Ingredients to pick" + ingredientsToPick);
     let orderName = document.createElement('p');
     table.appendChild(orderName);
     orderName.innerText = (`${choice.name}:`);//grabs name from chosen sandwich
     let ingredients = document.createElement('ul'); //makes a new list of ingredients
     orderName.appendChild(ingredients); //adds it to the table 
-    for (let i = 0; i < ingredientsToPick.length; i++){ //adds items to that list of ingredients
+    for (let i = 0; i < ingredientsToPick.length; i++) { //adds items to that list of ingredients
         let ingredient = document.createElement('li');
         ingredient.innerText = ingredientsToPick[i]; //fills it with text 
         ingredients.appendChild(ingredient); //puts on screen
     }
-   
+
 }
 
 
 
-function play(){
-    table.innerText="";
+function play() {
+    totalRounds += 1;
+    table.innerText = "";
     chooseSandwich();
-    for (let i = 0; i < foodButtons.length; i++){ //checks chosen buttons against winning buttons and adds matching ones to new array
-        foodButtons[i].addEventListener('click', function clickedItems(){
-            for (let b = 0; b < ingredientsToPick.length; b++){
-                if (foodButtons[i].innerText === ingredientsToPick[b]){
+    for (let i = 0; i < foodButtons.length; i++) { //checks chosen buttons against winning buttons and adds matching ones to new array
+        foodButtons[i].addEventListener('click', function clickedItems() {
+            for (let b = 0; b < ingredientsToPick.length; b++) {
+                if (foodButtons[i].innerText === ingredientsToPick[b]) {
                     chosenIngredients.push(ingredientsToPick[b]);
-                    console.log ("Chosen Ingredients " +   chosenIngredients);
-                    // let picOfChosen = document.createElement('img');
-                    // picOfChosen.innerHTML = foodButtons[i].style.backgroundImage;
-                    // workBoard.appendChild(picOfChosen)
-                }
-            }
-            ingredientsToPick.sort();
-            chosenIngredients.sort();
-            console.log ("Ingredients to pick " + ingredientsToPick);
-            console.log ("Chosen Ingredients " +   chosenIngredients);
-            let checkVal = true;
-            for (let i = 0; i < ingredientsToPick.length; i ++){
-                if (ingredientsToPick[i] === chosenIngredients[i]) { 
-                    checkVal = true;//if every element in our new array is selected, 
-            }else{
-                    checkVal = false;
-                    i = ingredientsToPick.length;
-            }}
-            if (checkVal){    
-                submitButton.addEventListener('click', function addTip(){
-                    tip.innerText = (`Your tip: ${currentTip + 5}`);
-                })
         
-            }
-        })
+                    //     let picOfChosen = document.createElement('img');
+                    //      picOfChosen.innerHTML = foodButtons[i].style.backgroundImage;
+                    //      console.log(picOfChosen);
+                    //      workBoard.appendChild(picOfChosen)
+                    // }
+                }}
+                ingredientsToPick.sort();
+                chosenIngredients.sort();
+                finalIngredients = [...new Set(chosenIngredients)]; //removes duplicates
+                for (let i = 0; i < ingredientsToPick.length; i++) {
+                    if (ingredientsToPick[i] === finalIngredients[i]) {
+                        checkVal = true;//if every element in our new array is selected, 
+                    } else {
+                        checkVal = false;
+                        i = ingredientsToPick.length;
+                    }
+                }
+            
+                if (checkVal === true && ingredientsToPick.length === finalIngredients.length) {
+                    console.log ("checkval is running")
+                    submitButton.style.opacity = 1;
+                    submitButton.style.borderColor = "red";
+                    submitButton.addEventListener('click', function addTip() {
+                        submitButton.style.opacity = .7;
+                        submitButton.style.borderColor = "white";
+                        currentTip += 5;
+                        chosenIngredients = [];
+                        tiptext.innerText = (`Your Tip: $${currentTip}`);
+                        play();
+                    })
+
+                }
+            })
+            
+      
     }
-  
-    
 }
+// if (totalRounds < 6){
+
+
+// }else{
+//     table.innerText = "Great work! You have many happy customers, but lunch is around the corner. Would you like to continue at this pace, or work the lunch rush?"
+// }
+
+
 
 table.addEventListener('click', play);
