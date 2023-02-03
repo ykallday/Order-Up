05687afe70d2bb2baba64f59ed05f72d0cb016fe
Yael Kaufman <ylkauf@gmail.com>
2023-02-03 11:28:25 -0800

    updates

--- js/app.js
+++ js/app.js
@@ -16,10 +16,13 @@ const pastrami = document.getElementById("pastrami");
 const sprouts = document.getElementById("sprouts");
 const capers = document.getElementById("capers");
 const submitButton = document.getElementById("buttonCell");
+const tip = document.getElementById("tip");
 const foodButtons = [bagel, tomato, lox, onion, avocado ,special, bacon, creamcheese, 
     swiss, egg, vegegg, usa, cheddar, pastrami, sprouts, capers]
-let ingredientsToPick;
-
+const ingredientsToPick=[];
+const chosenIngredients=[];
+const workBoard = document.getElementById("current-ingredients");
+const currentTip = 0;
 
 const sandwiches = [
     {name:'The Classic', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion']},
@@ -35,27 +38,37 @@ function chooseSandwich (){
     const orderName = document.createElement('p');
     table.appendChild(orderName);
     orderName.innerText = (`${choice.name}:`);//grabs name from chosen sandwich
-    const ingredients = document.createElement('ul');
-    orderName.appendChild(ingredients);
-    for (let i = 0; i < choice.ingredients.length; i++){
+    const ingredients = document.createElement('ul'); //makes a new list of ingredients
+    orderName.appendChild(ingredients); //adds it to the table 
+    for (let i = 0; i < ingredientsToPick.length; i++){ //adds items to that list of ingredients
         let ingredient = document.createElement('li');
-        ingredient.innerText = choice.ingredients[i];
-        ingredients.appendChild(ingredient);
+        ingredient.innerText = choice.ingredients[i]; //fills it with text 
+        ingredients.appendChild(ingredient); //puts on screen
     }
-}
-for (let i = 0; i < foodButtons.length; i++){
-    
 }
 //
 function play(){
     table.innerText="";
     chooseSandwich();
-
+    for (let i = 0; i < foodButtons.length; i++){ //checks chosen buttons against winning buttons and adds matching ones to new array
+        foodButtons[i].addEventListener('click', function clickedItems(){
+            for (let b = 0; b < ingredientsToPick.length; b++){
+                if (foodButtons[i].innerText === ingredientsToPick[b]){
+                    chosenIngredients.push(ingredientsToPick[b]);
+                    let picOfChosen = document.createElement('img');
+                    picOfChosen.innerHTML = foodButtons[i].style.backgroundImage;
+                    workBoard.appendChild(picOfChosen)
+                }
+            }
+        })
     }
-    
-
-// }
-
+    for (let i = 0; i < ingredientsToPick.length; i ++){
+        if (ingredientsToPick[i].every(v => chosenIngredients.includes(v))) { //if every element in our new array is selected, 
+            submitButton.style.opacity=1;
+            submitButton.addEventListener('click', function addTip(){
+                tip.innerText = (`Your tip: ${currentTip + 5}`);
+            })
+        }
+    }
 }
-
 table.addEventListener('click', play);
