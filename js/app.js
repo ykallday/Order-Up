1b0be0c5ae7bdd3efef52e524d29435c21392d2d
Yael Kaufman <ylkauf@gmail.com>
2023-02-03 12:12:27 -0800

    still working through js logic to get win functions to work

--- js/app.js
+++ js/app.js
@@ -15,14 +15,14 @@ const cheddar = document.getElementById("cheddar");
 const pastrami = document.getElementById("pastrami");
 const sprouts = document.getElementById("sprouts");
 const capers = document.getElementById("capers");
-const submitButton = document.getElementById("buttonCell");
+const submitButton = document.getElementById("orderUp");
 const tip = document.getElementById("tip");
 const foodButtons = [bagel, tomato, lox, onion, avocado ,special, bacon, creamcheese, 
-    swiss, egg, vegegg, usa, cheddar, pastrami, sprouts, capers]
-const ingredientsToPick=[];
-const chosenIngredients=[];
+    swiss, egg, vegegg, usa, cheddar, pastrami, sprouts, capers];
+let ingredientsToPick=[];
+let chosenIngredients=[];
 const workBoard = document.getElementById("current-ingredients");
-const currentTip = 0;
+let currentTip = 0;
 
 const sandwiches = [
     {name:'The Classic', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion']},
@@ -33,20 +33,25 @@ const sandwiches = [
 //chooses a sandwich from the list, prints its name and ingredients
 function chooseSandwich (){
     let choose = Math.floor(Math.random() * sandwiches.length);
-    let choice = sandwiches [choose]; //picks sandwich, assigns to choice
+    console.log(choose);
+    let choice = sandwiches[choose]; //picks sandwich, assigns to choice
     ingredientsToPick = choice.ingredients;
-    const orderName = document.createElement('p');
+    console.log("Ingredients to pick" + ingredientsToPick);
+    let orderName = document.createElement('p');
     table.appendChild(orderName);
     orderName.innerText = (`${choice.name}:`);//grabs name from chosen sandwich
-    const ingredients = document.createElement('ul'); //makes a new list of ingredients
+    let ingredients = document.createElement('ul'); //makes a new list of ingredients
     orderName.appendChild(ingredients); //adds it to the table 
     for (let i = 0; i < ingredientsToPick.length; i++){ //adds items to that list of ingredients
         let ingredient = document.createElement('li');
-        ingredient.innerText = choice.ingredients[i]; //fills it with text 
+        ingredient.innerText = ingredientsToPick[i]; //fills it with text 
         ingredients.appendChild(ingredient); //puts on screen
     }
+   
 }
-//
+
+
+
 function play(){
     table.innerText="";
     chooseSandwich();
@@ -55,20 +60,34 @@ function play(){
             for (let b = 0; b < ingredientsToPick.length; b++){
                 if (foodButtons[i].innerText === ingredientsToPick[b]){
                     chosenIngredients.push(ingredientsToPick[b]);
-                    let picOfChosen = document.createElement('img');
-                    picOfChosen.innerHTML = foodButtons[i].style.backgroundImage;
-                    workBoard.appendChild(picOfChosen)
+                    console.log ("Chosen Ingredients " +   chosenIngredients);
+                    // let picOfChosen = document.createElement('img');
+                    // picOfChosen.innerHTML = foodButtons[i].style.backgroundImage;
+                    // workBoard.appendChild(picOfChosen)
                 }
+            }
+            ingredientsToPick.sort();
+            chosenIngredients.sort();
+            console.log ("Ingredients to pick " + ingredientsToPick);
+            console.log ("Chosen Ingredients " +   chosenIngredients);
+            let checkVal = true;
+            for (let i = 0; i < ingredientsToPick.length; i ++){
+                if (ingredientsToPick[i] === chosenIngredients[i]) { 
+                    checkVal = true;//if every element in our new array is selected, 
+            }else{
+                    checkVal = false;
+                    i = ingredientsToPick.length;
+            }}
+            if (checkVal){    
+                submitButton.addEventListener('click', function addTip(){
+                    tip.innerText = (`Your tip: ${currentTip + 5}`);
+                })
+        
             }
         })
     }
-    for (let i = 0; i < ingredientsToPick.length; i ++){
-        if (ingredientsToPick[i].every(v => chosenIngredients.includes(v))) { //if every element in our new array is selected, 
-            submitButton.style.opacity=1;
-            submitButton.addEventListener('click', function addTip(){
-                tip.innerText = (`Your tip: ${currentTip + 5}`);
-            })
-        }
-    }
+  
+    
 }
+
 table.addEventListener('click', play);
