af54dba9794c3c42ba8378d8ff5b9b8b54fb3b65
Yael Kaufman <ylkauf@gmail.com>
2023-02-08 11:06:01 -0800

    added audio

--- js/app.js
+++ js/app.js
@@ -1,51 +1,53 @@
 let table = document.querySelector("#upperText");
 let startButton = document.querySelector("openShop");
+let cellbuttons = document.querySelectorAll('.cell');
+let foodButtons = Array.from(cellbuttons);
+let workBoard = document.getElementById("current-ingredients");
+let timerSpot = document.getElementById("timer");
+const submitButton = document.getElementById("orderUp");
+const tip = document.getElementById("tiptext");
 let restartButton;
 let continueCheck;
 let iQuit;
 let goHome;
 let keepWorking;
 let timeLeft = 30;
-let timerSpot;
-let cellbuttons = document.querySelectorAll('.cell');
-let foodButtons = Array.from(cellbuttons);
-const submitButton = document.getElementById("orderUp");
-const tip = document.getElementById("tiptext");
 let ingredientsToPick = [];
 let doneIngredient;
 let chosenIngredients = [];
-let workBoard = document.getElementById("current-ingredients");
 let checkList;
 let currentTip = 0;
 let addedTip = 5;
 let totalRounds = 0;
 let checkVal = true;
-let finalIngredients = [];
-let level = 0;
+let level = 1;
+
+//array of sandwich objects, which each have an array of ingredients
 const sandwiches = [
     { name: 'The Classic', ingredients: ['Bagel', 'Cream Cheese', 'Lox', 'Tomato', 'Capers', 'Onion'] },
     { name: 'The Basic', ingredients: ['Bagel', 'Cream Cheese'] },
     { name: 'The Egg, Cheese & Bacon', ingredients: ['Bagel', 'Egg', 'American Cheese', 'Special Sauce', 'Avocado', 'Bacon'] },
     { name: 'The Vegan', ingredients: ['Bagel', 'Avocado', 'Sprouts', 'Tomato', 'Onion', 'Vegan Egg'] },
     { name: 'The Pastrami Breakfast', ingredients: ['Bagel', 'Pastrami', 'Egg', 'Swiss Cheese', 'Special Sauce'] },
-    { name: 'The Bacon-Schmear-Tomato', ingredients: ['Bagel', 'Cream Cheese', 'Tomato', 'Bacon' ]},
+    { name: 'The Bacon-Schmear-Tomato', ingredients: ['Bagel', 'Tomato', 'Cream Cheese','Bacon' ]},
     { name: 'Custom Order', ingredients: ['Bagel', 'Avocado', 'Tomato', 'Bacon' ]},
     { name: 'Custom Order', ingredients: ['Bagel', 'Egg', 'Tomato', 'American Cheese', 'Cheddar Cheese' ]},
-    { name: 'Custom Order', ingredients: ['Bagel', 'Cream Cheese', 'Tomato', 'Onion', 'Bacon' ]},
-    { name: 'Custom Order', ingredients: ['Bagel', 'Cream Cheese', 'Lox']}
+    { name: 'Custom Order', ingredients: ['Bagel', 'Tomato', 'Onion', 'Cream Cheese','Bacon' ]},
+    { name: 'Custom Order', ingredients: ['Bagel', 'Cream Cheese', 'Lox']},
+    { name: 'Reuben-Ish', ingredients: ['Bagel', 'Pastrami', 'Swiss Cheese', 'Special Sauce']}
 ]
 
 //chooses a sandwich from the list, prints its name and ingredients
 function chooseSandwich() {
-    ingredientsToPick=[];
+    ingredientsToPick=[]; //creates empty array of solution ingredients
     let choose = Math.floor(Math.random() * sandwiches.length); //selects index
     let choice = sandwiches[choose]; //selects sandwich using index, assigns to choice
-    ingredientsToPick = choice.ingredients; //assign array to value of that sandwiches ingredients
+    ingredientsToPick = choice.ingredients; //fills ingredientsToPick with sandwich's ingredients
     let orderName = document.createElement('p'); //create paragraph
     table.appendChild(orderName); //add it to the table
     orderName.innerText = (`${choice.name}:`);//grabs name from chosen sandwich and prints it
     let ingredients = document.createElement('ul'); //makes a new list of ingredients
-    orderName.appendChild(ingredients); //adds it to the table 
+    orderName.appendChild(ingredients); //adds general list to the table 
     for (let i = 0; i < ingredientsToPick.length; i++) { //adds items to that list of ingredients
         let ingredient = document.createElement('li');
         ingredient.innerText = ingredientsToPick[i]; //fills it with text 
@@ -54,35 +56,35 @@ function chooseSandwich() {
 
 }
 
-function checkIngredientExist(current){
+function checkIngredientExist(current){ //prevents multiples of same correct ingredient
     checkVal=true;
     for (let i = 0; i < chosenIngredients.length; i++){
         if (current === chosenIngredients[i]){
-            return checkVal = false;
+            return checkVal = false; //if our current chosen ingredient is the same as any ingredient we've already chosen, return false
         } else{
-            checkVal = true;
+            checkVal = true; //if we haven't selected it before, return true
         }
     }
-    if (checkVal){
-        chosenIngredients.push(current);  //add to array
-        doneIngredient = document.createElement("li");
-        doneIngredient.innerText = current;
-        checkList.appendChild(doneIngredient);
+    if (checkVal){ //if we haven't selected it before
+        chosenIngredients.push(current);  //add to our array of chosen ingredients
+        doneIngredient = document.createElement("li"); //create list item
+        doneIngredient.innerText = current; //assign it to the value of the current selected ingredient
+        checkList.appendChild(doneIngredient); //add it to our workboard (which our checklist has been added to already)
     }
 }
 
-function play() {
-    totalRounds += 1;
+function playGame() {
+    totalRounds += 1; //increase round count
     console.log("totalRounds " + totalRounds);
-    addedTip = 5;
-    table.innerText = "";
-    chooseSandwich();
-    if (level === 1){
+    addedTip = 5; //assign tip value
+    table.innerText = ""; //clear table
+    chooseSandwich(); 
+    if (level === 2){ 
         setUpTimer();
-        setInterval(levelOneTimer, 1000);
+        setInterval(levelTwoTimer, 1000);
         clearInterval();
     }
-    if (level === 2 && totalRounds < 5){
+    if (level === 3 && totalRounds < 5){
         setInterval(timedTips, 5000);
         clearInterval();
     } 
@@ -112,15 +114,15 @@ function playMore(){
     level += 1;
     console.log("new level " + level);
     totalRounds = 0;
-    play();
+    playGame();
 }
 
 function playReset(){
     totalRounds = 0;
     currentTip = 0;
-    level = 0;
+    level = 1;
     tiptext.innerText = (`Your Tip: $${currentTip}`);
-    play();
+    playGame();
 }
 
 function endGame(){
@@ -138,20 +140,26 @@ function endGame(){
     table.appendChild(iQuit);
     table.appendChild(closedSign);
     table.appendChild(restartButton);
-    level=0;
+    level=1;
+    
+    
 }
 
+
 function continuePlayCheck(){
-    if (timeLeft > 0 && level == 1 || totalRounds < 5 && level === 2 || level === 0 && totalRounds < 5){
-        play();
+    checkList.remove();
+    chosenIngredients=[];
+    ingredientsToPick=[]; 
+    if (timeLeft > 0 && level == 2 || totalRounds < 5 && level === 3 || level === 1 && totalRounds < 5){
+        playGame();
     } else {
         table.innerText="";
         continueCheck = document.createElement('p');
-        if (level === 0){
+        if (level === 1){
             continueCheck.innerText=(`You've done well, but the lunch rush is coming! In the next level, you'll only have 30 seconds to make as many sandwiches as possible. Do you want to continue, or take your $${currentTip} and clock out?`);}
-        else if (level === 1){
+        else if (level === 2){
             continueCheck.innerText = (`Great work! You've got an early bird dinner rush coming in. They are impatient, and your speed will matter. They'll tip lower for slow service. Do you want to continue, or take your $${currentTip} and clock out?`);
-        }else if (level === 2 && totalRounds >=5){
+        }else if (level === 3 && totalRounds >=5){
             return endGame();
         }
         table.appendChild(continueCheck);
@@ -167,22 +175,22 @@ function continuePlayCheck(){
 }
 
 function setUpTimer(){
-    timerSpot = document.createElement('div');
-    timerSpot.setAttribute('id', 'timer');
     timerSpot.innerText = timeLeft;
-    table.appendChild(timerSpot);
+    timerSpot.style.display="block";
+    
 }
 
 
-function levelOneTimer(){
-    if (timeLeft > 0 && level === 1){
+function levelTwoTimer(){
+    if (timeLeft > 0 && level === 2){
         timeLeft -=1;
         timerSpot.innerText = timeLeft;
         }
 
-    if (timeLeft <= 0 && level === 1){
+    if (timeLeft <= 0 && level === 2){
         timeLeft = 0;
         clearInterval();
+        timerSpot.style.display = "none";
         checkList.remove();
         continuePlayCheck();  
     }
@@ -197,7 +205,7 @@ function timedTips(){
 }
 }
 
-openShop.addEventListener('click', play);
+openShop.addEventListener('click', playGame);
 
 submitButton.addEventListener('click', function addTip(){
     submitButton.style.opacity = .7;
@@ -207,7 +215,6 @@ submitButton.addEventListener('click', function addTip(){
         currentTip+=addedTip;
         tiptext.innerText = (`Your Tip: $${currentTip}`);
         chosenIngredients=[];
-        checkList.remove();
         ingredientsToPick=[]; 
         continuePlayCheck();
 }
